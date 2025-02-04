bea1c871e57977cefb7d33bba0272a0a2e6e899e
MgstrMilitum <ajanzen91@gmail.com>
2025-02-03 21:42:46 -0500

    EntryCastle Start
    Level 1 begun

--- Assets/Scripts/Player/Climbing.cs
+++ Assets/Scripts/Player/Climbing.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class Climbing : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("References")]
+    public Transform orientation;
+    public Rigidbody rb;
+    public LayerMask whatIsWall;
+    [Header("Climbing")]
+    public float climbSpeed;
+    public float maxClimbTime;
+    public float climbTimer;
+
+    private bool climbing;
+
+    [Header("Detection")]
+    public float detectionLength;
+    public float sphereCastRadius;
+    public float maxWallLookAngle;
+    public float wallLookAngle;
+
+    private RaycastHit frontWallHit;
+    private bool wallFront;
 
     // Update is called once per frame
     void Update()
     {
-        
+        WallCheck();
     }
+
+    private void WallCheck()
+    {
+        wallFront = Physics.SphereCast(transform.position, sphereCastRadius, orientation.forward, out frontWallHit, detectionLength, whatIsWall);
+    }
+
+    
 }
