e6099e640982de4061f9c903569db0f2c382262a
LightChristinzio <LWChristinzio@student.fullsail.edu>
2025-02-13 18:52:22 -0500

    Damage Adjust
    Adjusted Damage Script (now "TrapDamage" script) and removed unnecessary serialized fields. TrapDamage script now only handles traps dealing damage to the player, and has a bool to determine whether damage is dealt once to the player upon entering the trigger, or over time as long as the player is in the trigger.

--- Assets/Scripts/Environment/TrapDamage.cs
+++ Assets/Scripts/Environment/TrapDamage.cs
@@ -3,36 +3,17 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class Damage : MonoBehaviour
+public class TrapDamage : MonoBehaviour
 {
-    enum DamageType
-    {
-        Moving,
-        Stationary,
-        Melee
-    }
-
-    [SerializeField] DamageType type;
-    [SerializeField] Rigidbody rb;
     [SerializeField] bool isLava;
 
     [SerializeField] int damageAmount;
-    [SerializeField] int speed;
-    [SerializeField] int destroyTime;
     [SerializeField] float damageDelay;
 
     float localDamageDelay;
 
-    public bool isAttacking;
-
     void Start()
     {
-        isAttacking = false;
-        rb.linearVelocity = transform.forward * speed;
-        if (type == DamageType.Moving)
-        {
-            Destroy(gameObject, destroyTime);
-        }
         localDamageDelay = damageDelay;
     }
 
@@ -48,11 +29,6 @@ public class Damage : MonoBehaviour
         if (dmg != null)
         {
             dmg.TakeDamage(damageAmount);
-        }
-
-        if (type == DamageType.Moving)
-        {
-            Destroy(gameObject);
         }
     }
 
