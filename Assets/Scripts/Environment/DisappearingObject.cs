80e0225a6bb5505d57a378bf42ffa1a20021b448
LightChristinzio <LWChristinzio@student.fullsail.edu>
2025-02-13 18:32:21 -0500

    More Environmental Scripts
    Added a Teleporter Script which moves the player's position when they walk into a collider, added a Checkpoint Script which record's the player's stats (Current Health and Shield, Max Health and Shield), added Disappearing Object script which causes an object to be disabled when the player enters a trigger, then re-enables the object after a set delay.Added a Teleporter Script which moves the player's position when they walk into a collider, added a Checkpoint Script which record's the player's stats (Current Health and Shield, Max Health and Shield), added Disappearing Object script which causes an object to be disabled when the player enters a trigger, then re-enables the object after a set delay.

---
+++ Assets/Scripts/Environment/DisappearingObject.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class DisappearingObject : MonoBehaviour
+{
+    [SerializeField] GameObject toBeDisappeared;
+    [SerializeField] float disappearDelay;
+    [SerializeField] float reappearDelay;
+
+    bool toBeReappeared;
+
+    float localDisappearDelay;
+    float localReappearDelay;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toBeReappeared)
+        {
+            if (localReappearDelay <= 0)
+            {
+                toBeDisappeared.SetActive(true);
+                toBeReappeared = false;
+                localDisappearDelay = reappearDelay;
+            }
+            else
+            {
+                reappearDelay -= Time.deltaTime;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            if (!toBeReappeared)
+            {
+                if (localDisappearDelay <= 0)
+                {
+                    toBeDisappeared.SetActive(false);
+                    toBeReappeared = true;
+                    localDisappearDelay = disappearDelay;
+                }
+                else
+                {
+                    disappearDelay -= Time.deltaTime;
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Environment/Teleporter.cs
+++ Assets/Scripts/Environment/Teleporter.cs
@@ -1,30 +1,25 @@
 using UnityEngine;
 
-public class GravityLift : MonoBehaviour
+public class Teleporter : MonoBehaviour
 {
-    [SerializeField] Vector3 JumpLiftVelocity;
-
-    Rigidbody rb;
-
-    float localVelocityDelay;
-
+    [SerializeField] Vector3 teleportLocation;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
-    void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag.Equals("Player"))
         {
-            other.gameObject.GetComponent<Rigidbody>().AddForce(JumpLiftVelocity);
+            other.transform.position = teleportLocation;
         }
     }
 }
