a9aae26e7630e29aa78fb388d806680f8ff6002b
imMrMadlad <toroalmodovars@gmail.com>
2025-02-19 13:59:31 -0500

    The start of the belta sprint

--- Assets/Scripts/Player/Movement/Grappling.cs
+++ Assets/Scripts/Player/Movement/Grappling.cs
@@ -52,7 +52,7 @@ public class Grappling : MonoBehaviour
 
     void Update()
     {
-        //Debug.Log("Gun tip active? " + gunTip.gameObject.activeSelf);
+        Debug.Log("Gun tip active? " + gunTip.gameObject.activeSelf);
         if (Input.GetKeyDown(GrappleKey))
             StartGrapple();
 
