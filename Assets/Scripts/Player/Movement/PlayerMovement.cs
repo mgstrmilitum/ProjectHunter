fbfe403a65341400c6603e79f54becae89c122c1
imMrMadlad <toroalmodovars@gmail.com>
2025-02-22 15:51:35 -0500

    Revert "Merge branch 'AustinBranch' into Samuel-dont-break-Branch"
    This reverts commit 7bd7d3dbc17c567c581df8f845988f69a0f44ec6, reversing
    changes made to f28b8f693c11b37ae30c0f916444ccb6d9eff036.

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -102,10 +102,6 @@ public class PlayerMovement : MonoBehaviour
 
     public Difficulty difficulty;
 
-    [SerializeField] GameObject playertransform;
-
-    public CapsuleCollider capsuleCollider;
-
     public enum MovementState
     {
         Freeze,
@@ -125,16 +121,11 @@ public class PlayerMovement : MonoBehaviour
         rb.freezeRotation = true;
         readyToJump = true;
         startYScale = transform.localScale.y;
-        capsuleCollider = playertransform.GetComponentInChildren<CapsuleCollider>();
-        
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        //difficulty easy
-        EasyMode();
         //ground check
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + groundCheckDistance, whatIsGround);
         MyInput();
@@ -166,7 +157,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void OnCollisionEnter(Collision collision)
-    {
+    { 
     }
 
     private void StateHandler()
@@ -262,13 +253,13 @@ public class PlayerMovement : MonoBehaviour
                 rb.AddForce(Vector3.down * 80f, ForceMode.Force);
         }
         // Regular ground movement
-        if (moveDirection.magnitude > 0.3f && !isPlayingSteps)
+        if(moveDirection.magnitude > 0.3f && !isPlayingSteps)
         {
             StartCoroutine(PlaySteps());
         }
         //review cross products so this makes sense
         moveDirection = transform.forward * -horizontalInput + transform.right * verticalInput;
-        moveDirection = Vector3.Cross(slopeHit.normal, -moveDirection);
+        moveDirection = Vector3.Cross(slopeHit.normal,-moveDirection);
 
         //if (OnSlope() && !exitingSlope)
         //{
@@ -280,7 +271,7 @@ public class PlayerMovement : MonoBehaviour
         //}
 
         if (grounded)
-        {
+        { 
             rb.AddForce(moveDirection * moveSpeed * 10f, ForceMode.Force);
         }
         // Air movement
@@ -295,7 +286,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void SpeedControl()
     {
-        if (activeGrapple) return;
+        if(activeGrapple) return;
 
         if (OnSlope() && !exitingSlope)
         {
@@ -350,7 +341,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void LeanLeft()
     {
-        if (Input.GetKeyDown(LeanLeftKey))
+        if(Input.GetKeyDown(LeanLeftKey))
         {
             leanLeft = true;
             leanRight = false;
@@ -375,7 +366,7 @@ public class PlayerMovement : MonoBehaviour
             leanRight = false;
         }
     }
-
+    
     private void LeanDown()
     {
         //if (Input.GetKey(LeanDownKey))
@@ -390,11 +381,11 @@ public class PlayerMovement : MonoBehaviour
         LeanLeft();
         LeanRight();
 
-        if (leanLeft)
+        if(leanLeft)
         {
             targetLean = leanAngle;
         }
-        else if (leanRight)
+        else if(leanRight)
         {
             targetLean = -leanAngle;
         }
@@ -420,7 +411,7 @@ public class PlayerMovement : MonoBehaviour
 
     public bool OnSlope()
     {
-        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + slopeGroundCheckDistance))
+        if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + slopeGroundCheckDistance))
         {
             float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
             return angle < maxSlopeAngle && angle != 0f;
@@ -453,7 +444,7 @@ public class PlayerMovement : MonoBehaviour
         float difference = Mathf.Abs(desiredMoveSpeed - moveSpeed);
         float startValue = moveSpeed;
 
-        while (time < difference)
+        while(time < difference)
         {
             moveSpeed = Mathf.Lerp(startValue, desiredMoveSpeed, time / difference);
             time += Time.deltaTime;
@@ -480,14 +471,5 @@ public class PlayerMovement : MonoBehaviour
         //GameManager.Instance.damagePanel.SetActive(true);
         yield return new WaitForSeconds(.1f);
         //GameManager.Instance.damagePanel.SetActive(false);
-    }
-
-    public void EasyMode()
-    {
-        if (difficulty == Difficulty.Easy)
-        {
-            capsuleCollider.height = 1;
-
-        }
     }
 }
