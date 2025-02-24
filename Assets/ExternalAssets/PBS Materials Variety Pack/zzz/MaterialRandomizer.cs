277716c9ac73762900fbd79cdfd008398cfd8989
MgstrMilitum <atjanzen@student.fullsail.edu>
2025-02-24 01:44:57 -0500

    Last minute fixes
    Some scripts had to be commented out for build

--- Assets/ExternalAssets/PBS Materials Variety Pack/zzz/MaterialRandomizer.cs
+++ Assets/ExternalAssets/PBS Materials Variety Pack/zzz/MaterialRandomizer.cs
@@ -2,25 +2,25 @@ using UnityEngine;
 using System.Collections;
 using UnityEditor;
 
-[CustomEditor(typeof(MaterialRandomizerScript))]
-public class MaterialRandomizer : Editor 
+//[CustomEditor(typeof(MaterialRandomizerScript))]
+public class MaterialRandomizer
 {
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
+    //public override void OnInspectorGUI()
+    //{
+    //    DrawDefaultInspector();
 
-        MaterialRandomizerScript myScript = (MaterialRandomizerScript)target;
-        if(GUILayout.Button("Find Materials"))
-        {
-            myScript.findMaterials();
-        }
-        if(GUILayout.Button("Find Spheres"))
-        {
-            myScript.findMaterialSpheres();
-        }
-        if(GUILayout.Button("Randomize Materials"))
-        {
-            myScript.randomizeMaterials();
-        }
-    }
+    //    MaterialRandomizerScript myScript = (MaterialRandomizerScript)target;
+    //    if(GUILayout.Button("Find Materials"))
+    //    {
+    //        myScript.findMaterials();
+    //    }
+    //    if(GUILayout.Button("Find Spheres"))
+    //    {
+    //        myScript.findMaterialSpheres();
+    //    }
+    //    if(GUILayout.Button("Randomize Materials"))
+    //    {
+    //        myScript.randomizeMaterials();
+    //    }
+    //}
 }
